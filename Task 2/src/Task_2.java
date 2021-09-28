c127926ef732fcab1175e266f6f846cf76481c75
Mikhail Kireyenka <mixail.55290@gmail.com>
2021-09-29 00:13:06 +0300

    Add file task-2

---
+++ Task 2/src/Task_2.java
@@ -0,0 +1,9 @@
+public class CLS {
+    public static void main(String[] args) {
+
+        // Task 2. Заменить в строке все вхождения 'word' на 'letter'
+
+        String str1 = "Hello world, brave new world";
+        System.out.println(str1.replace("world","letter"));
+    }
+}
